886ca146e8695f2c9505e48094e71f37b37af1ec
cmatiasc <guiamultimedia1989@gmail.com>
2023-08-07 13:10:59 -0300

    agrega clase Importar

---
+++ src/testextrasg1/Insertando.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testextrasg1;
+
+/**
+ *
+ * @author matias
+ */
+public class Insertando {
+    
+}
